b26854d78d3aa00bb116b5262af5abb70953715a
Vasilkova Lyubov <damirana_@mail.ru>
2025-09-18 12:29:52 +0700

    update work_time/dao.py

--- app/work_time/dao.py
+++ app/work_time/dao.py
@@ -2,6 +2,7 @@ from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from sqlalchemy.future import select
 from app.work_time.models import Work, Break
+from app.operator.models import Employee
 
 
 class WorkDAO (BaseDAO):
@@ -19,7 +20,16 @@ class WorkDAO (BaseDAO):
             if not work_info:
                 return None
 
-            return work_info
+            # Второй запрос для получения информации о работнике
+            query_operator = select(Employee).filter_by(id=work_info.operator_id)
+            result_operator = await session.execute(query_operator)
+            operator_info = result_operator.scalar_one()
+
+            work_data = work_info.to_dict()
+            # statistic_data['operator'] = operator_info.name
+            work_data['operator'] = f"{operator_info.surname} {operator_info.name}"
+
+            return work_data
 
 
 class BreakDAO (BaseDAO):
@@ -37,4 +47,13 @@ class BreakDAO (BaseDAO):
             if not break_info:
                 return None
 
-            return break_info
+            # Второй запрос для получения информации о работнике
+            query_operator = select(Employee).filter_by(id=break_info.operator_id)
+            result_operator = await session.execute(query_operator)
+            operator_info = result_operator.scalar_one()
+
+            break_data = break_info.to_dict()
+            # statistic_data['operator'] = operator_info.name
+            break_data['operator'] = f"{operator_info.surname} {operator_info.name}"
+
+            return break_data
